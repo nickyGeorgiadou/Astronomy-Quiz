cccb2dbb10c0b56e90794d93962103981005c857
nickyGeorgiadou <georgiadounicky@gmail.com>
2026-03-28 18:04:57 +0200

    Εφτιαξα την αρχη και καποιες ερωτησεις

---
+++ quiz_game.py
@@ -0,0 +1,118 @@
+print ("Welcome to my Astronomy quiz!")
+
+playing = input("Do you want to play? (yes/no) ")
+
+if playing != "yes":
+    quit()
+    
+print ("Perfect! Let's go! 🚀")
+
+
+#Questions:
+
+answer = input("How many planets are there in our solar system? ")
+
+if answer == "8":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which planet is the closest to the sun? ")
+
+if answer == "Mercury":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which planet in our Solar system is the hottest? ")
+
+if answer == "Venus":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which planet in our Solar system is the largest? ")
+
+if answer == "Jupiter":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("What is the speed of light? ")
+
+if answer == "300000 m/s":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which term describes the boundary around a black hole beyond which nothing, not even light, can escape? ")
+
+if answer == "Event Horizon":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("What type of galaxy is the Milky Way? ")
+
+if answer == "Spiral galaxy":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which astronomer formulated the three laws of planetary motion? (last name) ")
+
+if answer == "Kepler":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("What evolutionary stage will our Sun enter after it exhausts the hydrogen fuel in its core? ")
+
+if answer == "Red giant":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which planet in our solar system is known for rotating on its side, with an axial tilt of about 98 degrees? ")
+
+if answer == "Uranus":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which of Jupiter's moons is considered a prime candidate in the search for extraterrestrial life due to its suspected global subsurface ocean? ")
+
+if answer == "Europa":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("What is the most abundant element in the universe? ")
+
+if answer == "Hydrogen":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+answer = input("Which was the first manned mission to land on the moon? ")
+
+if answer == "Apollo 11":
+    print("Correct :)")
+else:
+    print("Incorrect :(")
+
+
+
+
